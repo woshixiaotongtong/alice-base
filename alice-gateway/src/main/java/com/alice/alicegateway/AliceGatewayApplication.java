2e9c1ef6a3bd8d12afdf5b8eb1f94311b0d38ffe
liuguinan <49757831+woshixiaotongtong@users.noreply.github.com>
2022-02-23 10:57:31 +0800

    first commit

---
+++ alice-gateway/src/main/java/com/alice/alicegateway/AliceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.alice.alicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliceGatewayApplication.class, args);
+    }
+
+}
---
+++ alice-gateway/src/test/java/com/alice/alicegateway/AliceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alice.alicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
