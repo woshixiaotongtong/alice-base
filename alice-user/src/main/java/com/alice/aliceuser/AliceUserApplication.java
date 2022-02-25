03469e1d94bc366c277ca6277e48f229ff7b59b9
liuguinan <49757831+woshixiaotongtong@users.noreply.github.com>
2022-02-25 14:48:03 +0800

    文件修改

---
+++ alice-user/src/main/java/com/alice/aliceuser/AliceUserApplication.java
@@ -0,0 +1,13 @@
+package com.alice.aliceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliceUserApplication.class, args);
+    }
+
+}
